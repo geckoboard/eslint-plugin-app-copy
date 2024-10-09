7ff874e6ee31bf7835f612ef4f439c2a037c46a7
Leo Cassarani <me@leocassarani.net>
2024-10-09 16:19:27 +0100

    Export the NoApologies rule from index.js
    Co-authored-by: Ian Wright <ian@geckoboard.com>

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const NoApologies = require("./src/no-apologies");
 const PreferQuot = require("./src/prefer-quot");
 
 module.exports = {
   rules: {
+    "no-apologies": NoApologies,
     "prefer-quot": PreferQuot,
   }
 };
