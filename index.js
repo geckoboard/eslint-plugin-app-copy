ac8be2fcf2b0d1beb877ce357798771e23c759af
Ian Wright <ian@geckoboard.com>
2026-04-30 13:42:43 +0100

    Add exports

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const NoApologies = require("./src/no-apologies");
 const PreferRsquo = require("./src/prefer-rsquo");
+const UseEMDash = require("./src/use-em-dash");
+const UseENDash = require("./src/use-en-dash");
+const UseHyphen = require("./src/use-hyphen");
 
 module.exports = {
   rules: {
     "no-apologies": NoApologies,
     "prefer-rsquo": PreferRsquo,
+    "use-em-dash": UseEMDash,
+    "use-en-dash": UseENDash,
+    "use-hyphen": UseHyphen,
   }
 };
